38ffd31e62156a40e9a1f972d3e82e26866697a9
fnsilva97-stack <fnsilva97@gmail.com>
2026-06-17 12:42:51 -0500

    Delete estudio-grabacion directory

--- estudio-grabacion/public/js/config.js
+++
@@ -1,9 +0,0 @@
-// Configuración general de la aplicación
-const APP_CONFIG = {
-  DOMINIO_PERMITIDO: '@unimagdalena.edu.co',
-  HORA_APERTURA: 7,   // 7:00 AM
-  HORA_CIERRE: 20,    // 8:00 PM
-  DIAS_OPERACION: [1, 2, 3, 4, 5], // lunes a viernes (1=lunes ... 5=viernes)
-  DURACION_RESERVA_HORAS: 2,
-  ANTICIPACION_MINIMA_HORAS: 24
-};
